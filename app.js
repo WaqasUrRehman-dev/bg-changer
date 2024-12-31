8e5507f9a9a3f4a859f79adbc52852476d4e4332
Waqas Ur Rehman <rehmanwaqas466@gmail.com>
2024-12-31 22:20:16 +0500

    change background color after every second

---
+++ app.js
@@ -0,0 +1,26 @@
+const randomColor = function () {
+  let hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+console.log(randomColor());
+
+let interval;
+const startChangingColor = function () {
+  interval = setInterval(changeBgColor, 1000);
+
+  function changeBgColor() {
+    document.body.style.backgroundColor = randomColor();
+  }
+};
+
+const stopChangingColor = function () {
+  clearInterval(interval);
+};
+
+document.querySelector("#start").addEventListener("click", startChangingColor);
+document.querySelector("#stop").addEventListener("click", stopChangingColor);
